bf9d3d10d2cc5dd57c87571ce71350a444d7bf4e
ripeplantain <irvingmanny@gmail.com>
2024-01-26 12:09:02 +0000

    cd ../..

---
+++ Ecommerce.Common/src/Ecommerce.Common/Redis/RedisExtension.cs
@@ -0,0 +1,24 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+
+
+namespace Ecommerce.Common.Redis
+{
+
+
+
+    public static class RedisExtension
+    {
+        public static IServiceCollection AddRedis(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = configuration.GetConnectionString("Redis");
+                options.InstanceName = "master";
+            });
+
+            return services;
+        }
+    }
+}
